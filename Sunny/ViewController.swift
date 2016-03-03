94eac1468a1a50c8f332273a0314514e74d07144
Raghav Sai Cheedalla <raghavsaicheedalla@raghavs-macbook-pro.local>
2016-03-02 19:15:32 -0500

    Initial Commit

---
+++ Sunny/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sunny
+//
+//  Created by Raghav Sai Cheedalla on 3/2/16.
+//  Copyright © 2016 Bidkraft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
